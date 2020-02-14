2581f3221e8558e90ef66dbadf660e9820be0b44
michelle-hey <miscmiche@gmail.com>
2020-02-14 00:15:43 -0500

    MVP met

--- README.md
+++ README.md
@@ -23,32 +23,32 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 
 * [x] Verify that you have LESS installed correctly by running `lessc -v` in your terminal, if you don't get a version message back, reach out to your project manager for help.
 
-* [ ] Open your terminal and navigate to your preprocessing project by using the `cd` command
+* [x] Open your terminal and navigate to your preprocessing project by using the `cd` command
 
-* [ ] Once in your project's root folder, run the following command `less-watch-compiler less css index.less`
+* [x] Once in your project's root folder, run the following command `less-watch-compiler less css index.less`
 
-* [ ] Verify your compiler is working correctly by changing the `background-color` on the `html` selector to `red` in your `index.less` file.
+* [x] Verify your compiler is working correctly by changing the `background-color` on the `html` selector to `red` in your `index.less` file.
 
-* [ ] Once you see the red screen, you can delete that style and you're ready to start on the next task
+* [x] Once you see the red screen, you can delete that style and you're ready to start on the next task
 
 ## Task 2: Create Unique Event Listeners
 
 * [ ] Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). using your creativity to make the Fun Bus site more interactive.  Here are some unique events you could try to use: 
-	* [ ] `mouseover`
-	* [ ] `keydown`
-	* [ ] `wheel`
-	* [ ] `drag / drop`
+	* [x] `mouseover`
+	* [x] `keydown`
+	* [x] `wheel`
+	* [x] `drag / drop`
 	* [ ] `load`
 	* [ ] `focus`
 	* [ ] `resize`
 	* [ ] `scroll`
 	* [ ] `select`
-	* [ ] `dblclick`
+	* [x] `dblclick`
 
 Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
 
-* [ ] Nest two similar events somewhere in the site and prevent the event propagation properly
-* [ ] Stop the navigation items from refreshing the page by using `preventDefault()`
+* [x] Nest two similar events somewhere in the site and prevent the event propagation properly
+* [x] Stop the navigation items from refreshing the page by using `preventDefault()`
 
 ## Stretch Task:
 
@@ -57,3 +57,13 @@ Using the 10 unique events, find ways to update the DOM in creative ways. For ex
 ## Stretch assignment
 
 * [ ] Take a look at the [stretch assignment](stretch-assignment) and follow the instructions in the read me file.
+
+
+<!-- Questions to ask: 
+
+1. How do you know when to run less, sass, yarn, npm or other programs without being directed to? 
+2. When to use const, let, or var (basic question)
+3. How do you make the alert box go away after it comes up?
+4. when to use "" or ''
+
+-->
--- js/index.js
+++ js/index.js
@@ -1,2 +1,163 @@
-// Your code goes here
-// code here
+// Mouseover
+const navHov= document.querySelectorAll('.nav-link');
+navHov.forEach(function(anchor){
+    anchor.addEventListener('mouseenter', function(event){
+        event.target.style.color= 'yellow';
+        event.target.style.textTransform= 'uppercase';
+        event.target.style.transform = "scale(1.2)";
+    })    
+    anchor.addEventListener('mouseleave', function(event){
+        event.target.style.color= '';
+        event.target.style.textTransform= '';
+        event.target.style.transform = "scale(1.2)";
+    })
+})
+
+
+// click event, applied to multiple example
+document.querySelectorAll("h2").forEach((el) => {
+    el.addEventListener("click", function() {
+        el.style.color="hotpink";
+    })
+})
+
+// * [ ] `keydown`
+const body = document.querySelector('body');
+body.addEventListener('keydown', (e) => {
+        body.style.backgroundColor = "lightblue";
+    })
+
+// * [ ] `keyup`
+const body2 = document.querySelector('body');
+body.addEventListener('keyup', (e) => {
+        body.style.backgroundColor = '';
+    })
+
+// * [ ] `wheel` this is if wheeling while hovering over "fun bus" words in nav area
+const container = document.querySelector('.container');
+console.log(container)
+container.addEventListener('wheel', (e) => {
+    container.style.backgroundColor='red'
+    })
+
+// * [ ] `drag / drop` how do you make the alert box go away after click?
+const busPic = document.querySelector(".intro img");
+busPic.addEventListener('drag', (e) => {
+    alert('You are such a drag queen.')
+}, false);
+
+
+// * [ ] `load`
+// window.addEventListener('load', (event) => {
+//     console.log('page is fully loaded');
+//     }); 
+
+// * [ ] `double click` (doesn't work with QSAll)
+
+const changeImg = document.querySelector('.img-content img');
+
+changeImg.addEventListener('dblclick', function (e) {
+  changeImg.src = "img/citylights.jpg";
+});
+
+
+
+// * [ ] `focus`
+        // el.addEventListener('focus', ()=>{
+        //     el.style.background ='pink';
+        // })
+        // el.addEventListener('blur', ()=>{
+        //     el.style.background = '';
+        // });
+// * [ ] `blur`
+
+// password.addEventListener('focus', (event) => {
+    // event.target.style.background = 'pink';    
+    // });
+
+    // password.addEventListener('blur', (event) => {
+    // event.target.style.background = '';    
+    // }); 
+
+
+// * [ ] `resize` window.addEventListener('resize', reportWindowSize);
+// here, when window resizes image will change, in example
+// const newVar = document.querySelector("");
+// window.addEventListener('resize', () => {
+//     newVar.src="";
+// })
+
+// const bigPic = document.querySelector('.container home .intro img');
+// window.addEventListener('resize', () => {
+//         bigPic.src="../img/citylights.jpg";
+//     });
+
+
+
+// * [ ] `select` I don't understand the example
+
+    /** function logSelection(event) {
+     const log = document.getElementById('log');
+    const selection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
+    log.textContent = `You selected: ${selection}`;
+    }
+
+    const input = document.querySelector('input');
+    input.addEventListener('select', logSelection); */
+
+
+// * [ ] `scroll`
+
+        // window.addEventListener('scroll', () =>{
+        //     const scrolled = window.scrollX;
+        // });
+
+    // to throttle scroll event, use requestAnimationFrame(), setTimeout(), or CustomEvent, example below
+        /** 
+
+        let last_known_scroll_position = 0;
+        let ticking = false;
+
+        function doSomething(scroll_pos) {
+        // Do something with the scroll position
+        }
+
+        window.addEventListener('scroll', function(e) {
+        last_known_scroll_position = window.scrollY;
+
+        if (!ticking) {
+            window.requestAnimationFrame(function() {
+            doSomething(last_known_scroll_position);
+            ticking = false;
+            });
+
+            ticking = true;
+        }
+        }); */
+
+
+
+
+// * [ ] Nest two similar events somewhere in the site and prevent the event propagation properly
+// What does it mean by nesting similar events? 
+
+    const para = document.querySelector('.text-content p');
+    para.addEventListener('click', (e) => {
+        console.log('clicked the paragraph');
+    })
+
+    const textDiv = document.querySelector('.text-content');
+    textDiv.addEventListener('click', (event) => {
+        console.log('Clicked div');
+    })
+
+
+// * [ ] Stop the navigation items from refreshing the page by using `preventDefault()`
+
+const stopTheLink = document.querySelector('.nav-link');
+stopTheLink.addEventListener('click', (event) => {
+    console.log('Stopping the link!');
+      if (true === false) {
+      event.preventDefault();
+    }
+  })
